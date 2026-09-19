2c9cea6fa5633f24d1f8f34620874fd26931bfb6
Jiali <jiali123@uw.edu>
2026-09-18 17:22:25 -0700

    updates

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
@@ -23,5 +24,14 @@ public class HelloController {
     @GetMapping("/health")
     public String health() {
         return "OK";
+    }
+
+    @GetMapping("/message")
+    public String getMessage() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(
+            "http://demo2:8080/internal/message",
+            String.class
+        );
     }
 }
